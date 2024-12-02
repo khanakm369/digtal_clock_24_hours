5694a27ba79813545a7e05a354521da972730b0f
khanakm369 <git@stackblitz.com>
2024-12-02 23:35:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'; 
+
+import './App.css';
+
+function App() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (time) => {
+    const hours = String(time.getHours()).padStart(2, '0');
+    const minutes = String(time.getMinutes()).padStart(2, '0');
+    const seconds = String(time.getSeconds()).padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  return (
+    <>
+      <div className="container">{formatTime(time)}</div>
+    </>
+  );
+}
+
+export default App;
